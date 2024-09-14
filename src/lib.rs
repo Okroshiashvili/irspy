ab33c1661e784730fcf88da7c6ca1d1e110ca9e4
Nodar Okroshiashvili <n.okroshiashvili@gmail.com>
2024-09-14 23:54:45 +0400

    Add XML file reader

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,25 @@
 use pyo3::prelude::*;
+// use roxmltree::Document;
+use std::fs::File;
+use std::io::Read;
 
-/// Formats the sum of two numbers as string.
 #[pyfunction]
-fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
-    Ok((a + b).to_string())
+fn read_xml_file(file_path: &str) -> PyResult<String> {
+    let mut file = File::open(file_path)?;
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)?;
+    Ok(contents)
 }
+
+// #[pyfunction]
+// fn parse_xml_file(xml_string: &str) -> Result<Document, roxmltree::Error> {
+//     let doc = Document::parse(xml_string)?;
+//     Ok(doc)
+// }
 
 /// A Python module implemented in Rust.
 #[pymodule]
 fn irspy(m: &Bound<'_, PyModule>) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+    m.add_function(wrap_pyfunction!(read_xml_file, m)?)?;
     Ok(())
 }
